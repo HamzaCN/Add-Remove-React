08a3f2d3127296d80c89cdf74ef1d306698914b1
HamzaCN <rajpoothamza923@gmail.com>
2022-03-27 21:20:14 +0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react";
+import "./index.css";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+
+  const [num, setNum] = useState(0);
+
+  const incNum = () =>{
+    setNum(num+1);
+  }
+
+  const decNum = () =>{
+
+    if(num>0){
+      setNum(num-1);
+    }
+    else{
+      alert("Zero Reached");
+      setNum(0);
+    }
+  }
+
+
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1> {num} </h1>
+          <div className="btn_div">
+          <Tooltip title="Add .">
+            <button onClick={incNum} className="green"> <AddCircleOutlineIcon/> </button>
+            </Tooltip>
+
+<Tooltip title="Delete">
+            <button onClick={decNum} className="red"> <RemoveCircleOutlineIcon /></button>
+            </Tooltip>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
